61407fce5dc05125085ed4bda5923617dbd541b9
danielulisses <danielulisses@outlook.com>
2024-05-21 16:45:11 -0300

    moved to nord theme

--- init.lua
+++ init.lua
@@ -783,17 +783,22 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'sainnhe/sonokai',
+    'gbprod/nord.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
+      require('nord').setup {}
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'sonokai'
+      vim.o.termguicolors = true
+      vim.cmd.colorscheme 'nord'
 
       -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
+      -- vim.cmd.hi 'Comment gui=none'
     end,
+    install = {
+      colorscheme = { 'nord' },
+    },
   },
 
   -- Highlight todo, notes, etc in comments
---
+++ lua/custom/plugins/nord.lua
@@ -0,0 +1,3 @@
+return {
+  { 'gbprod/nord.nvim', name = 'nord', priority = 1000 },
+}
